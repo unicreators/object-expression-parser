3ddf1adbc3f3a91e97d77c72b21f46b1042daa32
yichen <dicksonworld@gmail.com>
2017-04-23 14:11:03 +0800

    深度重构
    1. 重新整理了代码结构，更清晰
    2. 增加了一些新的配置属性(暂未完成)
    3. 将通用功能提取为独立的模块

--- index.js
+++ index.js
@@ -1,20 +1,5 @@
 //////////////////////////////
 ///  yichen
 
-const {
-    ArgumentError,
-    OperatorError,
-    OperatorConfigurationError,
-    OperatorNotFoundError,
-    IncongruentRuleError
-} = require('./lib/error'),
-    ExpressionParser = require('./lib/index');
+module.exports = require('./lib/index');
 
-module.exports = ExpressionParser;
-
-module.exports.ExpressionParser = ExpressionParser;
-module.exports.ArgumentError = ArgumentError;
-module.exports.OperatorError = OperatorError;
-module.exports.OperatorConfigurationError = OperatorConfigurationError;
-module.exports.OperatorNotFoundError = OperatorNotFoundError;
-module.exports.IncongruentRuleError = IncongruentRuleError;
